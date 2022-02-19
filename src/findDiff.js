59bad0ea3f4447de27f14aa3d532992824af7813
Kristina <kristinakazlovskaya405.1@gmail.com>
2022-02-19 13:19:10 +0300

    refactor: refactor findDiff function

--- src/findDiff.js
+++ src/findDiff.js
@@ -4,24 +4,25 @@ const findDiff = (file1, file2) => {
   const keys = _.union(_.keys(file1), _.keys(file2));
 
   const diff = keys.map((key) => {
-    if (!_.has(file2, key)) {
-      return { name: key, value: file1[key], state: 'removed' };
-    }
-    if (!_.has(file1, key)) {
-      return { name: key, value: file2[key], state: 'added' };
-    }
-    if (_.isObject(file2[key]) && _.isObject(file1[key])) {
-      return { name: key, state: 'nested', children: findDiff(file1[key], file2[key]) };
-    }
-    if (file2[key] !== file1[key]) {
-      return {
+    const states = {
+      removed: () => ({ name: key, value: file1[key], state: 'removed' }),
+      added: () => ({ name: key, value: file2[key], state: 'added' }),
+      nested: () => ({ name: key, state: 'nested', children: findDiff(file1[key], file2[key]) }),
+      updated: () => ({
         name: key,
         previousValue: file1[key],
         currentValue: file2[key],
         state: 'updated',
-      };
-    }
-    return { name: key, value: file1[key], state: 'unchanged' };
+      }),
+      unchanged: () => ({ name: key, value: file1[key], state: 'unchanged' }),
+    };
+
+    const isRemoved = !_.has(file2, key) ? states.removed() : null;
+    const isAdded = !_.has(file1, key) ? states.added() : null;
+    const isNested = _.isObject(file2[key]) && _.isObject(file1[key]) ? states.nested() : null;
+    const isUpdated = file2[key] !== file1[key] ? states.updated() : null;
+
+    return isRemoved ?? isAdded ?? isNested ?? isUpdated ?? states.unchanged();
   });
 
   return _.sortBy(diff, 'name');
